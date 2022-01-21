ddb2613f03a8661e76ac5a427de2b29a5c966914
AlexAkulov <alexakulov86@gmail.com>
2022-01-22 01:43:12 +0500

    commit

--- words.js
+++ words.js
@@ -1,38 +1,14 @@
 const words = `
 
-
-Game changer
-Disruptive technology
-Bilateral
-Bandwidth
-Transparency
-Strategic fit
-Core competencies
-Out of the box
-Bottom line
-Revisit
-Take that off-line
-24/7
-Out of the loop
-Benchmark
-Value-added
-Thought leader
-Proactive
-Win-win
-Fast track
-Result-driven
-Empower (or empowerment)
-Knowledge base
-At the end of the day
-Touch base
-Mindset
-Client focus(ed)
-Ballpark
-Game plan
-Leverage
-Cascade
-Sequential or sequentially
-Multiple times
+Jira
+Zabbix/Nagios
+Salt/Puppet
+Documentation instead of automatisation
+Baremetal
+lxc
+Jenkins/TeamCity/Bamboo/GoCD
+Capistrano
+Bash and perl scrips
 
 
 `;
